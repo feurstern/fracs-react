14b85d621494adb5b733455230c9d63f1c408392
Denny <dennygunawan99@gmail.com>
2023-05-30 16:18:31 +0700

    Merapihkan code tag p line 12,13

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -8,9 +8,10 @@ const Footer = () => {
       <div className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
         <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
           <div className='flex-[-1] flex flex-col justify-start mr-10'>
-          <img src={frac_logo} alt='frac logo' className='w-[266px] h-[72px] object-contain'/>
-          <p className={`${styles.paragraph} mt-4 max-w-[312px]`}>
-            This is where the footer will be appear        </p>
+            <img src={frac_logo} alt='frac logo' className='w-[266px] h-[72px] object-contain'/>
+            <p className={`${styles.paragraph} mt-4 max-w-[312px]`}>
+              This is where the footer will be appear
+            </p>
           </div>
         </div>
       </div>
