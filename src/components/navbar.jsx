a048681c06b65080f13afc825910acede997d6f1
Rio <60057220+feurstern@users.noreply.github.com>
2023-05-30 09:46:14 +0700

    Update navbar.jsx
    add navbar by using array map

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,10 +2,28 @@ import React from 'react'
 import { useState } from 'react'
 import { navLinks } from '../constant'
 
+
+
 const Navbar = () => {
+  const[active,setActive] = useState("Home");
+  
   return (
     <nav className='w-full flex py-6 justify-between items-center navbar'>
         <img src='' alt="FRACS" className='w-[124px h-[32px]' />
+
+        <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px] ${
+              active === nav.title ? "text-white" : "text-dimWhite"
+            } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
+            onClick={() => setActive(nav.title)}
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
     </nav>
     
   )
