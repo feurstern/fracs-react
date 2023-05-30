62a9814db20ad82ab21e15c35cd8ea46690c5774
Rio <60057220+feurstern@users.noreply.github.com>
2023-05-30 14:35:08 +0700

    Add some changes for loop by using array map

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,7 +5,9 @@ import { navLinks } from '../constant'
 
 
 const Navbar = () => {
+  //create useState to change the condition
   const[active,setActive] = useState("Home");
+  const[toggle, setToggle] = useState(false);
   
   return (
     <nav className='w-full flex py-6 justify-between items-center navbar'>
@@ -24,6 +26,36 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+
+      <div className='sm:hidden flex flex-1 justify-end items-center'>
+            <img src='' alt='menu' 
+              className='w-28 h-28 object-contain'
+             onClick={()=>setToggle(!toggle)}
+            />
+        
+        
+            <div
+            className={`${
+              !toggle ? "hidden" : "flex"
+            } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+          >
+            <ul className="list-none flex justify-end items-start flex-1 flex-col">
+              {navLinks.map((nav, index) => (
+                <li
+                  key={nav.id}
+                  className={`font-poppins font-medium cursor-pointer text-[16px] ${
+                    active === nav.title ? "text-white" : "text-dimWhite"
+                  } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
+                  onClick={() => setActive(nav.title)}
+                >
+                  <a href={`#${nav.id}`}>{nav.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+
+      </div>
     </nav>
     
   )
