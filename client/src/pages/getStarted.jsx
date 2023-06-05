4e06376a3f2bd85aa382e371b64d24c1ae73e141
feurstern <naziincolour123@gmail.com>
2023-06-05 15:24:48 +0700

    added script for button get started

--- client/src/pages/getStarted.jsx
+++ client/src/pages/getStarted.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import styles from '../style'
+import { arrow_up } from '../Assets'
 
 const GetStarted = () => {
   return (
-    <div>
-      
+    <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient`}>
+      <div className={`${styles.flexCenter} flex-col bg-red-600 w-[100%] h-[100%] rounded-full`}>
+        <div className={`${styles.flexStart} flex-row`}>
+          <p className='font-poppins font-medium text-[18px] leading-[23px]'>
+             Get
+          </p>
+          <img src={arrow_up} alt="arrow up" className='w-[23px] h-[23.4px] object-contain' />
+        </div>
+        <p className='font-poppins font-medium text-[18px] leading-[23.4px]'>
+          <span className='text-gradient'>Started</span>
+        </p>
+      </div>
     </div>
   )
 }
