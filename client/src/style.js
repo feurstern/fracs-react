56633f008d567ddd5a97ce89d17f073709d5675d
feurstern <naziincolour123@gmail.com>
2023-06-06 15:21:29 +0700

    add new variable named as sectionBackReverse

--- client/src/style.js
+++ client/src/style.js
@@ -19,7 +19,7 @@ const styles = {
   export const layout = {
     section: `flex md:flex-row flex-col ${styles.paddingY}`,
     sectionReverse: `flex md:flex-row flex-col-reverse ${styles.paddingY}`,
-  
+    sectionBackReverse :`flex md:flex-row flex-col-reverse ${styles.paddingX}`,  
     sectionImgReverse: `flex-1 flex ${styles.flexCenter} md:mr-10 mr-0 md:mt-0 mt-10 relative`,
     sectionImg: `flex-1 flex ${styles.flexCenter} md:ml-10 ml-0 md:mt-0 mt-10 relative`,
   
