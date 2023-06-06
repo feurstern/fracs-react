05cf3d41d4556bd6de112c991736ef11355bc2a7
feurstern <naziincolour123@gmail.com>
2023-06-06 15:07:45 +0700

    add text-justfy for the paragraph's variable

--- client/src/style.js
+++ client/src/style.js
@@ -2,7 +2,7 @@ const styles = {
     boxWidth: "xl:max-w-[1280px] w-full",
   
     heading2: "font-poppins font-semibold xs:text-[48px] text-[40px] text-black xs:leading-[76.8px] leading-[66.8px] w-full",
-    paragraph: "font-poppins font-normal text-black text-[18px] leading-[30.8px]",
+    paragraph: "font-poppins font-normal text-black text-[18px] leading-[30.8px] text-justify",
 
     defaultHeaderFont : "font-poppins font-semibold text-3xl text-white text-dimWhite",
     flexCenter: "flex justify-center items-center",
