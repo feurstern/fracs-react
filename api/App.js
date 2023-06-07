b44cd94ee699864f97eaeab4272128d41a7fab67
feurstern <naziincolour123@gmail.com>
2023-06-07 13:53:27 +0700

    installed express and testing it

---
+++ api/App.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+
+app.get('/', (req,res)=>{
+    res.send("Connected succsefully");
+});
+
+app.listen(port,()=>{
+    console.log(`The port used is ${port}`);
+})
+
+}`);
+})
+
+}`);
+})
+
