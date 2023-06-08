c001a75c0ca0390d0520396a2de79240dda4c445
feurstern <naziincolour123@gmail.com>
2023-06-08 11:09:02 +0700

    add pages components from section of home page to the main page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Section1 from './Home/section-1'
 import Section2 from './Home/section-2'
+import Section3 from './Home/section-3'
+import Section4 from './Home/section-4'
 
 const Home = () => {
   return (
     <div>
         <h1>Home page</h1>
-        <Section1/>
-        <Section2/>
+        <Section1/> {/* header about IFMI*/}
+        <Section2/> {/* About curriculum */}
+        <Section3/> {/* curriculum scheme */}
+        <Section4/> {/* certififcation fee */}
     </div>
   )
 }
