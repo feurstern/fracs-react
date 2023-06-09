b5e76a3d026133283f47a6ed9acce973d4db5cbf
feurstern <naziincolour123@gmail.com>
2023-06-09 10:11:36 +0700

    added section-5 page

---
+++ client/src/pages/Home/section-5.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../../style'
+
+const Section5 = () => {
+  return (
+    <section className={`${styles.paddingY} ${styles.paddingY}`}>
+      
+    </section>
+  )
+}
+
+export default Section5
