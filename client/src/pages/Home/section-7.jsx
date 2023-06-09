2c2de824f369e0cd9bff7a6542826f47d8b3da7b
feurstern <naziincolour123@gmail.com>
2023-06-09 16:33:28 +0700

    commit some changes

--- client/src/pages/Home/section-7.jsx
+++ client/src/pages/Home/section-7.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import styles,{layout} from '../../style'
+import React from "react";
+import styles, { layout } from "../../style";
 
 const Section7 = () => {
   return (
-    <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex flex-row flex-col bg-black-radient-2 rounded-[20px] box-shadow`}>
-        <div className={`flex-1 flex flex-col`}>
-          <h2 className={`${styles.heading2}`}>
-            Let's Register to Our Program!
-          </h2>
-          <p className={`${styles.paragraph}`}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
-        </div>
-            
+    <section
+      className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex flex-row flex-col bg-black-radient-2 rounded-[20px] box-shadow`}
+    >
+      <div className={`flex-1 flex flex-col`}>
+        <h2 className={`${styles.heading2}`}>Let's Register to Our Program!</h2>
+        <p className={`${styles.paragraph}`}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
+      <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Section7
+export default Section7;
