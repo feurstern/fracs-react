f4507f461afbd5a27a27fa6fa55865dcf284ef39
Rio <naziincolour123@gmail.com>
2023-06-14 11:05:26 +0700

    create main component layout for register page

--- migrate-next-js/src/app/register/page.js
+++ migrate-next-js/src/app/register/page.js
@@ -1,13 +1,29 @@
 import React from 'react'
 import Section1reg from './section-1-reg'
 import { Navbar, Footer } from '../components/components'
+import styles from '../style'
 
 const Register = () => {
   return (
-    <div>
-      <Navbar/>
-      <Section1reg/>      
-      <Footer/>
+    <div className='bg-red-700 w-full overflow-hidden'>
+     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar/>
+      </div>
+     </div>
+
+     <div className={`bg-white border-red-600 ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+      </div>
+     </div>
+
+     <div className={`bg-white  ${styles.paddingX} ${styles.flexStart}`}>
+       <div className={`${styles.boxWidth}`}>
+        <Section1reg/>
+
+        <Footer/>
+       </div>
+     </div>
     </div>
   )
 }
