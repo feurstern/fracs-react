df255365e91a6386f04b67b09bce6c2766731359
Rio <naziincolour123@gmail.com>
2023-07-05 15:45:20 +0700

    add animation style to cta section

--- migrate-next-js/src/app/certifications/cta.jsx
+++ migrate-next-js/src/app/certifications/cta.jsx
@@ -1,10 +1,38 @@
+'use client'
 import React from 'react'
 import styles from '../style'
+import { animate, motion, useAnimation, useInView } from 'framer-motion'
+import { useState, useEffect, useRef } from 'react'
 
 const Cta = () => {
+    const ref = useRef(null);
+    const [hovered, setHovered] = useState(false);
+    const [isClicked, setIsClicked] = useState(false);
+    const control = useAnimation();
+    const isInView = useInView(ref,{once:true});
+    
+    useEffect(()=>{
+        isInView? control.start('visible') : control.start('hidden');
+    }, [isInView, control]);
+
+
   return (
-    <section class="bg-white">
-    <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+    <motion.section 
+        ref={ref}
+        variants={{
+            hidden: {opacity: 0, y:100},
+            visible : {opacity:1, y:0}
+        }}
+        initial='hidden'
+        animate ={control}
+        transition={{duration:1.5}}
+        class="bg-white"
+        >
+    <div 
+        onMouseEnter={()=>setHovered(true)}
+        onmouseLeave ={()=>setHovered(false)}
+        onClick={()=>setIsClicked(true)}
+        class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
         <a href="/register" class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
             <span class="text-xs bg-primary-600 rounded-full text-white px-4 py-1.5 mr-3">Register here</span> <span class="text-sm font-medium">Become rofessional</span> 
             <svg class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
@@ -23,7 +51,7 @@ const Cta = () => {
         </div>
       
     </div>
-</section>
+</motion.section>
   )
 }
 
