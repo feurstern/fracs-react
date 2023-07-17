c0d5c23e0916f869f89ec5d10b214907eb47afc9
Rio <naziincolour123@gmail.com>
2023-07-17 16:33:54 +0700

    create data contant for the detail, reveal for triggering hte modal, sectionload for set the state of the modal

--- migrate-next-js/src/app/gallery/modal/Modal.jsx
+++ migrate-next-js/src/app/gallery/modal/Modal.jsx
@@ -1,22 +1,61 @@
-import React, { useRef, useState } from 'react'
-import motion, { useInView } from 'framer-motion'
+import React, { useEffect, useRef, useState } from 'react'
+import  ReactDOM  from 'react-dom';
+import { motion, useAnimation, useInView } from 'framer-motion'
+import Reveal from './Reveal';
+import { title } from 'process';
 
 
-const Modal = () => {
+export const Modal = ({
+    modalContent,
+    isOpen,
+    title,
+}) => {
     const ref = useRef(null)
-    const control = Animation();
+    const control = useAnimation();
 
-    const [click, isClicked] = useState(false);
-    const isInView = useInView(ref, {once:true});
-    
+    const getHTMLBody = document.querySelector('body');
+
+    const [hovered, setHovered] = useState(false);
+
+    const isInView = useInView(ref, { once: true });
+
+    useEffect(() => {
+        isInView ? control.start('visible') : control.start('hidden')
+    })
+
     return (
         <motion.div
             ref={ref}
+            variants={{
+                hidden: { opacity: 0, y: 100 },
+                visible: { opacity: 2, y: 0 }
+            }}
+            initial='hidden'
+            animate={control}
+            transition={{ duration: 0.75 }}
+
             className=""
         >
+            <div
+                onMouseEnter={() => setHovered(true)}
+                onMouseLeave={() => setHovered(false)}
+                onClick={(e) =>e.stopPropagation }
+            >
+            </div>
 
+            <div>
+               <h4>{title}</h4>
+               <div>{modalContent}</div>
+            </div>
         </motion.div>
-    )
+    );
+
+    isOpen ? <></> : 0;
+
+    return ReactDOM.createPortal(content, document.getElementById('root'));
+
 }
 
-export default Modal
+
+
+
