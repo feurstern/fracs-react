ecca9899bea16e85216bbcb4af2442e0b5286026
Rio <naziincolour123@gmail.com>
2023-07-20 14:21:27 +0700

    add some script for the modal

--- migrate-next-js/src/app/gallery/modal/Modal.jsx
+++ migrate-next-js/src/app/gallery/modal/Modal.jsx
@@ -9,54 +9,68 @@ export const Modal = ({
     modalContent,
     isOpen,
     title,
+    setIsOpen
 }) => {
     const ref = useRef(null)
     const control = useAnimation();
 
-    const getHTMLBody = document.querySelector('body');
 
     const [hovered, setHovered] = useState(false);
 
     const isInView = useInView(ref, { once: true });
 
+
+
     useEffect(() => {
-        isInView ? control.start('visible') : control.start('hidden')
-    })
+        const getHTMLBody = document.querySelector('body');
+        isOpen ? getHTMLBody.style.overflowY = 'hidden' : 0
+        !isOpen ? getHTMLBody.style.overflowY = 'scroll' : 0
+    }, [isOpen])
 
-    const content =(
+    console.log(`The value of HTML BODY : ${getHTMLBody}`)
 
-    
-        <motion.div
-            ref={ref}
-            variants={{
-                hidden: { opacity: 0, y: 100 },
-                visible: { opacity: 2, y: 0 }
-            }}
-            initial='hidden'
-            animate={control}
-            transition={{ duration: 0.75 }}
+    const content = (
+        <div className={``} onClick={() => setIsOpen(false)}>
+            <button classname="">
+                <MdClose />
+            </button>
+            <motion.div
+                ref={ref}
+                variants={{
+                    hidden: { opacity: 0, y: 100 },
+                    visible: { opacity: 2, y: 0 }
+                }}
+                initial='hidden'
+                animate={control}
+                transition={{ duration: 0.75 }}
 
 
-            className=""
-        >
-            <div
-                onMouseEnter={() => setHovered(true)}
-                onMouseLeave={() => setHovered(false)}
-                onClick={(e) => e.stopPropagation}
+                className=""
             >
-            </div>
+                <div
+                    onMouseEnter={() => setHovered(true)}
+                    onMouseLeave={() => setHovered(false)}
+                    onClick={(e) => e.stopPropagation}
+                >
+                </div>
+                <div>
+                    <h4>{title}</h4>
+                    <div>{modalContent}</div>
+                    <div>{desc}</div>
+                </div>
+            </motion.div>
 
-            <div>
-                <h4>{title}</h4>
-                <div>{modalContent}</div>
-            </div>
-        </motion.div>
+        </div>
+
     )
 
     isOpen ? <></> : 0;
     return ReactDOM.createPortal(content, document.getElementById('root'));
-
 }
+
+
+
+
 
 
 
--- migrate-next-js/src/app/gallery/section-2-ga.jsx
+++ migrate-next-js/src/app/gallery/section-2-ga.jsx
@@ -4,14 +4,13 @@ import styles, { layout } from '../style'
 import Image from 'next/image'
 import { business } from '../Assets'
 import { motion, useAnimation, useInView } from 'framer-motion'
-import Reveal from './modal/Reveal'
-import {Modal} from './modal/Modal'
+import {Reveal} from './modal/Reveal'
+import { Modal } from './modal/Modal'
 
 export const Section2ga = ({
     modalContent,
     description,
     title,
-
 }) => {
     const hiddenMask = `repeating-linear-gradient(to right, rgba(0,0,0,0) 0px, rgba(0,0,0,0) 30px, rgba(0,0,0,1) 30px, rgba(0,0,0,1) 30px)`;
     const visibleMask = `repeating-linear-gradient(to right, rgba(0,0,0,0) 0px, rgba(0,0,0,0) 0px, rgba(0,0,0,1) 0px, rgba(0,0,0,1) 30px)`;
@@ -48,93 +47,95 @@ export const Section2ga = ({
                     <button type='button' className='pl-[10px] pr-[10px]'>Offline Training</button>
                     <button type='button' className='pl-[10px] pr-[10px]'>Online Training</button>
                 </div>
-                <div className={`${styles.galleryImageContainer} cursor-pointer pt-[20px] pb-[20px]`}>
-                    <motion.div
-                        whileHover={{ scale: 1.2 }}
-                        whileTap={{ scale: 0.8 }}
-                        onClick={() => setIsOpen(true)}
-                        className='hover:bg-red-700'>
-                        <img
-                            className='h-auto max-w-full rounded-lg hover:opacity-50'
-                            src="https://rjcons.com/wp-content/uploads/photo-gallery/2%20(1).jpg" />
-                    </motion.div>
-                    <motion.div
-                        whileHover={{ scale: 1.2 }}
-                        whileTap={{ scale: 0.8 }}
-                        onClick={() => setIsOpen(true)}
-                        className='hover:bg-red-700'>
-                        <img
-                            className='h-auto max-w-full rounded-lg hover:opacity-50'
-                            src="https://rjcons.com/wp-content/uploads/photo-gallery/2%20(1).jpg" />
-                    </motion.div>
-                    <motion.div
-                        whileHover={{ scale: 1.2 }}
-                        whileTap={{ scale: 0.8 }}
-                        onClicK ={()=> setIsOpen(true)}
-                        className='hover:bg-red-700'>
-                        <img
-                            className='h-auto max-w-full rounded-lg hover:opacity-50'
-                            src="https://rjcons.com/wp-content/uploads/photo-gallery/2%20(1).jpg" />
-                    </motion.div>
-                    <motion.div
-                        whileHover={{ scale: 1.2 }}
-                        whileTap={{ scale: 0.8 }}
-                        onClick={()=> setIsOpen(false)}
-                        className='hover:bg-red-700'>
-                        <img
-                            className='h-auto max-w-full rounded-lg hover:opacity-50'
-                            src="https://rjcons.com/wp-content/uploads/photo-gallery/2%20(1).jpg" />
-                    </motion.div>
-                    <motion.div
-                        whileHover={{ scale: 1.2 }}
-                        whileTap={{ scale: 0.8 }}
-                        onClicK={()=>setIsOpen(false)}
-                        className='hover:bg-red-700'>
-                        <img
-                            className='h-auto max-w-full rounded-lg hover:opacity-50'
-                            src="https://rjcons.com/wp-content/uploads/photo-gallery/2%20(1).jpg" />
-                    </motion.div>
-                    <motion.div
-                        whileHover={{ scale: 1.2 }}
-                        whileTap={{ scale: 0.8 }}
-                        onClick={()=>setIsOpen(false)}
-                        className='hover:bg-red-700'>
-                        <img
-                            className='h-auto max-w-full rounded-lg hover:opacity-50'
-                            src="https://rjcons.com/wp-content/uploads/photo-gallery/2%20(1).jpg" />
-                    </motion.div>
-                    <motion.div
-                        whileHover={{ scale: 1.2 }}
-                        whileTap={{scale :0.8}}
-                        onClick={()=>setIsOpen(false)}
-                        className='hover:bg-red-700'>
-                        <img
-                            className='h-auto max-w-full rounded-lg hover:opacity-50'
-                            src="https://rjcons.com/wp-content/uploads/photo-gallery/2%20(1).jpg" />
-                    </motion.div>
-                    <motion.div
-                        whileHover={{ scale: 1.2 }}
-                        whileTap={{ scale: 0.8 }}
-                        onClick={()=>setIsOpen(false)}
-                        className='hover:bg-red-700'>
-                        <img
-                            className='h-auto max-w-full rounded-lg hover:opacity-50'
-                            src="https://rjcons.com/wp-content/uploads/photo-gallery/2%20(1).jpg" />
-                    </motion.div>
-                    <motion.div
-                        whileHover={{ scale: 1.2 }}
-                        whileTap={{ scale: 0.8 }}
-                        onClick={()=>setIsOpen(false)}
-                        className='hover:bg-red-700'>
-                        <img
-                            className='h-auto max-w-full rounded-lg hover:opacity-50'
-                            src="https://rjcons.com/wp-content/uploads/photo-gallery/2%20(1).jpg" />
-                    </motion.div>
-                </div>
+                <Reveal width='100%' >
+                    <div className={`${styles.galleryImageContainer} cursor-pointer pt-[20px] pb-[20px]`}>
+                        <motion.div
+                            whileHover={{ scale: 1.2 }}
+                            whileTap={{ scale: 0.8 }}
+                            onClick={() => setIsOpen(true)}
+                            className='hover:bg-red-700'>
+                            <img
+                                className='h-auto max-w-full rounded-lg hover:opacity-50'
+                                src="https://rjcons.com/wp-content/uploads/photo-gallery/2%20(1).jpg" />
+                        </motion.div>
+                        <motion.div
+                            whileHover={{ scale: 1.2 }}
+                            whileTap={{ scale: 0.8 }}
+                            onClick={() => setIsOpen(true)}
+                            className='hover:bg-red-700'>
+                            <img
+                                className='h-auto max-w-full rounded-lg hover:opacity-50'
+                                src="https://rjcons.com/wp-content/uploads/photo-gallery/2%20(1).jpg" />
+                        </motion.div>
+                        <motion.div
+                            whileHover={{ scale: 1.2 }}
+                            whileTap={{ scale: 0.8 }}
+                            onClicK={() => setIsOpen(true)}
+                            className='hover:bg-red-700'>
+                            <img
+                                className='h-auto max-w-full rounded-lg hover:opacity-50'
+                                src="https://rjcons.com/wp-content/uploads/photo-gallery/2%20(1).jpg" />
+                        </motion.div>
+                        <motion.div
+                            whileHover={{ scale: 1.2 }}
+                            whileTap={{ scale: 0.8 }}
+                            onClick={() => setIsOpen(false)}
+                            className='hover:bg-red-700'>
+                            <img
+                                className='h-auto max-w-full rounded-lg hover:opacity-50'
+                                src="https://rjcons.com/wp-content/uploads/photo-gallery/2%20(1).jpg" />
+                        </motion.div>
+                        <motion.div
+                            whileHover={{ scale: 1.2 }}
+                            whileTap={{ scale: 0.8 }}
+                            onClicK={() => setIsOpen(false)}
+                            className='hover:bg-red-700'>
+                            <img
+                                className='h-auto max-w-full rounded-lg hover:opacity-50'
+                                src="https://rjcons.com/wp-content/uploads/photo-gallery/2%20(1).jpg" />
+                        </motion.div>
+                        <motion.div
+                            whileHover={{ scale: 1.2 }}
+                            whileTap={{ scale: 0.8 }}
+                            onClick={() => setIsOpen(false)}
+                            className='hover:bg-red-700'>
+                            <img
+                                className='h-auto max-w-full rounded-lg hover:opacity-50'
+                                src="https://rjcons.com/wp-content/uploads/photo-gallery/2%20(1).jpg" />
+                        </motion.div>
+                        <motion.div
+                            whileHover={{ scale: 1.2 }}
+                            whileTap={{ scale: 0.8 }}
+                            onClick={() => setIsOpen(false)}
+                            className='hover:bg-red-700'>
+                            <img
+                                className='h-auto max-w-full rounded-lg hover:opacity-50'
+                                src="https://rjcons.com/wp-content/uploads/photo-gallery/2%20(1).jpg" />
+                        </motion.div>
+                        <motion.div
+                            whileHover={{ scale: 1.2 }}
+                            whileTap={{ scale: 0.8 }}
+                            onClick={() => setIsOpen(false)}
+                            className='hover:bg-red-700'>
+                            <img
+                                className='h-auto max-w-full rounded-lg hover:opacity-50'
+                                src="https://rjcons.com/wp-content/uploads/photo-gallery/2%20(1).jpg" />
+                        </motion.div>
+                        <motion.div
+                            whileHover={{ scale: 1.2 }}
+                            whileTap={{ scale: 0.8 }}
+                            onClick={() => setIsOpen(false)}
+                            className='hover:bg-red-700'>
+                            <img
+                                className='h-auto max-w-full rounded-lg hover:opacity-50'
+                                src="https://rjcons.com/wp-content/uploads/photo-gallery/2%20(1).jpg" />
+                        </motion.div>
+                    </div>
+                </Reveal>
             </motion.section>
-            <Modal
+            {/* <Modal
                 modalContent={modalContent}
-            />
+            /> */}
         </>
 
     )
