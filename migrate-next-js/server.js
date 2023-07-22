5ea62a4b2313e955982d6b56b6857d5fec178a32
Rio <naziincolour123@gmail.com>
2023-07-22 13:09:43 +0700

    breaks out routing handling from next-server, next-dev-server, and base-server

--- migrate-next-js/server.js
+++ migrate-next-js/server.js
@@ -3,8 +3,8 @@ const { parse } = require('url')
 const next = require('next')
  
 const dev = process.env.NODE_ENV !== 'production'
-const hostname = 'localhost'
-const port = process.env.PORT || '3017'
+const hostname = 'https://dev.fracs.id/'
+const port = process.env.PORT || '3017' || '3000'
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port })
 const handle = app.getRequestHandler()
