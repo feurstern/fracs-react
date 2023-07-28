02bc70900fce884b3d9df3a8213f1eadca2d69c5
Rio <naziincolour123@gmail.com>
2023-07-28 16:11:59 +0700

    implement auto transition for image slider

--- migrate-next-js/src/app/gallery/Image-slide-new.jsx
+++ migrate-next-js/src/app/gallery/Image-slide-new.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { image } from './modal/image-data.js'
+
 const Imageslidenew = () => {
 
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -12,6 +13,18 @@ const Imageslidenew = () => {
     const nextImage = () => {
         setCurrentImageIndex((prevIndex) => (prevIndex + 1) % image.length);
     };
+
+    const autoTransition = () => {
+        nextImage();
+    }
+
+    useEffect(() => {
+        const interval = setInterval(autoTransition, 100000);
+
+        return () => {
+            clearInterval(interval);
+        }
+    }, [])// just remember to declare empty array to make sure the effects only run once when it renders
 
 
 
