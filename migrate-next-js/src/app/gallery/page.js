6f65500d6ebbeefbc2500da24a034660953fb72a
Rio <rio-kurniawan@sakura-schutzstaffel.com>
2023-08-11 13:44:27 +0700

    change the gallery page

--- migrate-next-js/src/app/gallery/page.js
+++ migrate-next-js/src/app/gallery/page.js
@@ -5,6 +5,7 @@ import { Footer, Navbar } from '../components/components'
 import Section1ga from './section-1-ga'
 import { Section2ga } from './section-2-ga'
 import Imageslidenew from './Image-slide-new'
+import Page404 from './404'
 
 const Gallery = () => {
 
@@ -23,11 +24,11 @@ const Gallery = () => {
 
       <div className={`bg-white ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          {/* <Page404/> */}
-          <Section1ga />{/*title of the page*/}
+          <Page404/>
+          {/* <Section1ga /> */}
           {/* <Imageslide /> */}
-          <Imageslidenew/>
-          <Section2ga />{/* image container */}
+          {/* <Imageslidenew/> */}
+          {/* <Section2ga />image container */}
           {/* <Footer/> */}
         </div>
       </div>
