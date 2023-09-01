a34d5517cbb439e1daf7663203d68a59f1717335
梨央Rio <rio-kurniawan@sakura-schutzstaffel.com>
2023-09-01 15:11:37 +0700

    implemented a new logic to convert the first letter to uppercase when the section has sub title

--- migrate-next-js/src/app/certifications/Section-1-cer-new.jsx
+++ migrate-next-js/src/app/certifications/Section-1-cer-new.jsx
@@ -4,23 +4,31 @@ import CollapseSc1 from './CollapaseSc1'
 
 const Section1cernew = () => {
     const rawTitle = 'persyaratan sertifikasi frac';
-    const arr = rawTitle.split(' ');
+    const subTitleRaw= 'berikut adalah persyaratan sertifikasi frac:'
     const firstIndex = 0;
     const sliceIndex = 1;
 
-    for(let i =0; i<arr.length; i++){
-        arr[i].charAt(firstIndex) === 'f' ? arr[i]= arr[i].toUpperCase() : arr[i] = arr[i].charAt(firstIndex).toUpperCase() + arr[i].slice(sliceIndex);
+
+    const titleUpperCase=( index, s, title)=>{
+
+      let arr= title.split(' ');
+      
+      for(let i =0; i<arr.length; i++){
+          arr[i].charAt(index) === 'f' ? arr[i]= arr[i].toUpperCase() : arr[i] = arr[i].charAt(index).toUpperCase() + arr[i].slice(s);
+      }
+      return arr.join(' ');
+      
     }
 
-    const title = arr.join(' ');
-
+    const title = titleUpperCase(firstIndex, sliceIndex, rawTitle);
+    const subTitle = titleUpperCase(firstIndex,sliceIndex, subTitleRaw);
   return (
      <section className={`px-4`}>
        <div>
           <h1 className={`${styles.heading2}`}>{title}</h1>
        </div>
        <div>
-        <h2 className={`${styles.heading3}`}>Persyaratan untuk menjadi Kandidat FRAC untuk dapat mengikuti ujian Sertifikasi FRAC adalah:</h2>
+        <h2 className={`${styles.heading3}`}>{subTitle}</h2>
        </div>
        <div className='px-8'>
         <CollapseSc1/>
