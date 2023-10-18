5fc0a80e74cb8470d4d92b616e514809d180cdae
梨央Rio <rio-kurniawan@sakura-schutzstaffel.com>
2023-10-18 15:45:20 +0700

    testing after moving to another device

---
+++ migrate-next-js/src/app/about-us/sections-1.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Section1tsx = () => {
+    let rawTitle: string = '';
+    let array: any = [];
+
+    return (
+        <>
+            <div>This is the page.</div>
+        </>
+    )
+
+}
+
+export default Section1tsx
